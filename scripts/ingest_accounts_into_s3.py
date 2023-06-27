e69d8fa62d037426bb2ac414189a2e12636e6ba6
João Paulo Nogueira <joaopcnogueira@gmail.com>
2023-06-27 20:01:26 -0300

    Update ingest_accounts_into_s3.py

--- scripts/ingest_accounts_into_s3.py
+++ scripts/ingest_accounts_into_s3.py
@@ -9,8 +9,8 @@ accounts = fake.generate_accounts(n_accounts)
 filename = datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + '-accounts.json'
 fake.save_into_s3(
     accounts,
-    bucket='missy-aulas-how',
-    key='bronze/desafio-01/' + filename,
+    bucket='008413171063-landing-zone',
+    key='desafio-01/' + filename,
     aws_access_key_id=os.getenv('AWS_ID'),
     aws_secret_access_key=os.getenv('AWS_KEY')
 )
