b4bf24da47bca4fcc4989b2568d197ea946690fa
João Nogueira <joao.nogueira@datarisk.io>
2023-07-04 13:43:35 -0300

    schedule rechanged to hourly

--- pipelines/ingestion_pipeline.py
+++ pipelines/ingestion_pipeline.py
@@ -4,11 +4,11 @@ import awswrangler as wr
 from datetime import datetime
 from how_fake_accounts import fake
 
-n_accounts = random.randint(500, 5000)
+n_accounts = random.randint(50, 500)
 accounts = fake.generate_accounts(n_accounts)
 
 sao_paulo_timezone = pytz.timezone('America/Sao_Paulo')
-partition_folder = "extracted_at=" + datetime.now(sao_paulo_timezone).strftime('%Y-%m-%d')
+partition_folder = "extracted_at=" + datetime.now(sao_paulo_timezone).strftime('%Y-%m-%dT%H:00:00')
 filename = 'accounts.parquet'
 
 wr.s3.to_parquet(
